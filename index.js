7434d4537551b5a137cf73c39a54ed24ac3513f5
sodmaq <sodmaq@gmail.com>
2025-02-22 22:19:49 +0100

    signup integration

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173, https://anonymous-rust.vercel.app",
+  origin: ["http://localhost:5173", "https://anonymous-rust.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
