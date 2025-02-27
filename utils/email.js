7bf7102dceb6d2db50ee62184504e834a8a45e1d
sodmaq <sodmaq@gmail.com>
2025-02-27 22:10:44 +0100

    message sort

--- utils/email.js
+++ utils/email.js
@@ -1,3 +1,32 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (options) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp.gmail.com",
+//       port: 465,
+//       secure: true,
+//       auth: {
+//         user: process.env.SMTP_EMAIL,
+//         pass: process.env.SMTP_PASSWORD,
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: "WhisperZone <yekeen244@gmail.com>",
+//       to: options.email,
+//       subject: options.subject,
+//       text: options.message,
+//       html: options.html,
+//     };
+
+//     await transporter.sendMail(mailOptions);
+//   } catch (error) {
+//     throw new Error("Email could not be sent.");
+//   }
+// };
+
+// module.exports = sendEmail;
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
@@ -10,18 +39,26 @@ const sendEmail = async (options) => {
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASSWORD,
       },
+      // Add this for better deliverability
+      pool: true,
+      rateDelta: 1000, // Space out emails by 1 second
+      rateLimit: 5, // Max 5 messages per second
     });
 
+    // Most important change - match "from" with your actual sending email
     const mailOptions = {
-      from: "WhisperZone <yekeen244@gmail.com>",
+      from: `"WhisperZone" <${process.env.SMTP_EMAIL}>`, // Must match your auth.user
       to: options.email,
       subject: options.subject,
       text: options.message,
       html: options.html,
+      // Optional but helpful
+      replyTo: process.env.SMTP_EMAIL,
     };
 
     await transporter.sendMail(mailOptions);
   } catch (error) {
+    console.error("Email error:", error);
     throw new Error("Email could not be sent.");
   }
 };
